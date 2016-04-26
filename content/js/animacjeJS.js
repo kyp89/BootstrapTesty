526adcab87398643f5bef31288ec7735445d120b
kyp89 <kyp1989@gmail.com>
2016-04-26 23:52:43 +0200

    Add first files. index.html, editPractise.html and content

---
+++ content/js/animacjeJS.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+  /*  $('#PractiseElement').hover(function(){
+       console.log("Wywolano"); 
+       $(this+'div').toggleClass('selectElement');
+       $(this+'div h3').toggleClass('selectHeader');
+    });*/
+
+});
